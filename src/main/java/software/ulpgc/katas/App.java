e175cf03cd5c03bdb55528dc7429b55f10fed60c
CAPi0807 <carlosalamocap@gmail.com>
2026-01-07 11:05:10 +0000

    Kata 2

--- src/main/java/software/ulpgc/katas/App.java
+++ src/main/java/software/ulpgc/katas/App.java
@@ -9,7 +9,6 @@ public class App {
         var cargador = new CargadorDatos();
         List<Empleado> empleados = cargador.cargarDesdeCSV("src/main/resources/datos.csv");
 
-
         var generator = new HistogramGenerator();
 
         Histogram histDepto = generator.compute(empleados, Empleado::departamento);
